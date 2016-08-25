ed2eef29926764af957b4204b52d30aa147a46e2
piyu.aryan@gmail.com <piyu.aryan@gmail.com>
2016-08-25 23:12:10 +1000

    The Login Page
    app.java: @configuration: Login page and all web resources made available by default
    hello.js: authenticate(): passed the basic in header and added the /user so that we can proceed in app.

--- src/main/java/com/example/UiApplication.java
+++ src/main/java/com/example/UiApplication.java
@@ -2,9 +2,15 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -17,11 +23,46 @@ public class UiApplication {
         SpringApplication.run(UiApplication.class, args);
     }
 
+    /**
+     * To service the authenticate() function we need to add a new endpoint to the backend:
+     * <p/>
+     * This is a useful trick in a Spring Security application.
+     * If the "/user" resource is reachable then it will return the currently authenticated user (an Authentication), and otherwise Spring Security will intercept the request
+     * and send a 401 response through an AuthenticationEntryPoint
+     *
+     * @param user
+     * @return
+     */
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+
     @RequestMapping("/resource")
     public Map<String, Object> home() {
         Map<String, Object> model = new HashMap<String, Object>();
         model.put("id", UUID.randomUUID().toString());
         model.put("content", "Hello World");
         return model;
+    }
+
+    /**
+     * Handling the Login Request on the Server
+     * <p/>
+     * This is a standard Spring Boot application with Spring Security customization,
+     * just allowing anonymous access to the static (HTML) resources (the CSS and JS resources are already accessible by default)
+     */
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .httpBasic()
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+                    .anyRequest().authenticated();
+        }
     }
 }
